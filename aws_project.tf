41650d6f4ab0a88af8c5bf127d57c5469eb58bc8
jaggudeva <165828944+jaggudeva@users.noreply.github.com>
2024-05-10 17:47:21 +0530

    Update aws_project.tf

--- aws_project.tf
+++ aws_project.tf
@@ -2,7 +2,7 @@ Provider "aws" {
  region = "us-east-1"
 }
 
-resource "aws_instance" "this" {
+resource "aws_instance" "example" {
   ami                     = "ami-04b70fa74e45c3917"
   instance_type           = "t2.micro"
 }
